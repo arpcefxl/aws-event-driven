9d0e34a386dc63cf5357e078725543f1339f1500
Chad Smith <csmith@Chads-MacBook-Pro.local>
2021-12-06 19:04:19 -0700

    completed demo 05

--- 05/consolidate_events.sh
+++ 05/consolidate_events.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #Consolidates EventBridge events into a single region
 CENTRALREGION=$1
-EVENTBUSARN=$2
-ACCTID=$3
-PROFILE=$4
+ACCTID=$2
+PROFILE=$3
 EVENTBUSARN="arn:aws:events:${CENTRALREGION}:${ACCTID}:event-bus/default"
 
 #Create an IAM role required for cross-region event target
-ROLEARN=`aws iam create-role --role-name EventBusConsolidationRole --assume-role-policy-document file://role_trust_policy.json --query Role.Arn`
+ROLEARN=`aws --region us-east-1 --output text --profile $PROFILE iam create-role --role-name EventBusConsolidationRole --assume-role-policy-document file://role_trust_policy.json --query Role.Arn`
+echo "role arn is $ROLEARN"
 
 #Substitute the variable in the template permission policy with the correct event bus ARN
 sed "s|EVENTBUSARN|${EVENTBUSARN}|g" role_permission_template.json > role_permission_policy.json
 
 #Create an IAM policy from the updated permission policy
-IAMPOLICYARN=`aws iam create-policy --policy-name EventBusConsolidationPolicy --policy-document file://role_permission_policy.json --query Policy.Arn`
-
+IAMPOLICYARN=`aws --region us-east-1 --output text --profile $PROFILE iam create-policy --policy-name EventBusConsolidationPolicy --policy-document file://role_permission_policy.json --query Policy.Arn`
+echo "iam policy ARN is $IAMPOLICYARN"
 #Attach the new IAM policy to the role
-aws iam attach-role-policy --role-name EventBusConsolidationRole --policy-arn $IAMPOLICYARN
+aws --region us-east-1 --output text --profile $PROFILE iam attach-role-policy --role-name EventBusConsolidationRole --policy-arn $IAMPOLICYARN
 
 
 #Create the EventBridge rule which forwards all events to the central region using the newly created IAM role
 
-REGIONS=`aws ec2 describe-regions --query Regions[].RegionName |tr -s '\t' '\n'`
+REGIONS=`aws --region us-east-1 --output text --profile $PROFILE ec2 describe-regions --query Regions[].RegionName |tr -s '\t' '\n'`
 for i in $REGIONS; do
 if [[ $i != $CENTRALREGION ]]; then
   echo "Creating rule in region $i"
---
+++ 05/delete_failed_rule.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Consolidates EventBridge events into a single region
+CENTRALREGION=$1
+ACCTID=$2
+PROFILE=$3
+EVENTBUSARN="arn:aws:events:${CENTRALREGION}:${ACCTID}:event-bus/default"
+
+#Create an IAM role required for cross-region event target
+#ROLEARN=`aws iam create-role --role-name EventBusConsolidationRole --assume-role-policy-document file://role_trust_policy.json --query Role.Arn`
+
+#Substitute the variable in the template permission policy with the correct event bus ARN
+#sed "s|EVENTBUSARN|${EVENTBUSARN}|g" role_permission_template.json > role_permission_policy.json
+
+#Create an IAM policy from the updated permission policy
+#IAMPOLICYARN=`aws iam create-policy --policy-name EventBusConsolidationPolicy --policy-document file://role_permission_policy.json --query Policy.Arn`
+
+#Attach the new IAM policy to the role
+#aws iam attach-role-policy --role-name EventBusConsolidationRole --policy-arn $IAMPOLICYARN
+
+
+#Create the EventBridge rule which forwards all events to the central region using the newly created IAM role
+
+REGIONS=`aws ec2 describe-regions --query Regions[].RegionName |tr -s '\t' '\n'`
+for i in $REGIONS; do
+if [[ $i != $CENTRALREGION ]]; then
+  echo "Creating rule in region $i"
+  aws --profile $PROFILE events --region $i delete-rule --name "EventConsolidation" 
+fi
+done
